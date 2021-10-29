2e563bba43cd4f7c0e04c9911bade4aef26b6306
3RSH/JeS <diz.domination@gmail.com>
2021-10-29 18:21:10 +0300

    version 1.0 complete

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -26,14 +27,67 @@ public class Application {
     long timeInit;
     long timeSQL;
     long time1xml = 0;
-    long time2xml = 0;
+    long time2xml;
     long timeSum;
     long timeAll;
+    long sum = 0;
 
+    readInnerData("data.properties");
+    timeInit = System.currentTimeMillis() - start;
+    System.out.println(
+        "Инициализация входных данных выполнена. Прошло " + timeInit / 1000 + " сек.");
 
-    File inData = new File("data.properties");
+    try (Connection connection = DriverManager.getConnection(
+        data.getUrlDb(), data.getUserDb(), data.getPasswordDb())) {
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(inData))){
+      Statement statement = connection.createStatement();
+
+      checkAndCleanDB(statement);
+      fillDB(statement);
+      timeSQL = System.currentTimeMillis() - (timeInit + start);
+      System.out.println("SQL-таблица заполнена. Прошло ещё " + timeSQL / 1000 + " сек.");
+
+      createXml(statement);
+      time1xml = System.currentTimeMillis() - (timeSQL + start);
+      System.out.println("1.xml создан. Прошло ещё " + time1xml / 1000 + " сек.");
+
+    } catch (SQLException e) {
+      System.err.println("Нет соединения с базой данных");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+
+      File transXml = transformXmlToXml("TransformXML.xsl");
+      time2xml = System.currentTimeMillis() - (time1xml + start);
+      System.out.println("2.xml создан. Прошло ещё " + time2xml / 1000 + " сек.");
+
+      Entries entries = parseXML(transXml);
+
+      for (Entry e : entries.entries) {
+        sum += e.field;
+      }
+
+      System.out.println("Сумма всех атрибутов fileld из файла 2.xml: " + sum);
+
+      timeSum = System.currentTimeMillis() - (time2xml + start);
+      System.out.println("Сумма подсчитана. Прошло ещё " + timeSum / 1000 + " сек.");
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    timeAll = System.currentTimeMillis() - start;
+    System.out.println("Всего прошло " + timeAll / 1000 + " сек.");
+
+    checkSum(sum);
+  }
+
+  private static void readInnerData(String propFile) {
+    File inData = new File(propFile);
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(inData))) {
 
       for (int i = 0; i < 4; i++) {
         String line = reader.readLine();
@@ -62,137 +116,96 @@ public class Application {
       }
     } catch (FileNotFoundException e) {
       System.err.println("Файл с исходными данными не найден.");
-    }catch (IllegalArgumentException e) {
+    } catch (IllegalArgumentException e) {
       System.err.println("Не найдены исходные данные.");
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
 
-    timeInit = System.currentTimeMillis() - start;
-    System.out.println("Инициализация входных данных выполнена. Прошло " + timeInit/1000 + " сек.");
+  private static void checkAndCleanDB(Statement statement) throws SQLException {
+    ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM \"TEST\"");
+    resultSet.next();
 
-    //    ==============
+    int size = Integer.parseInt(resultSet.getString(1));
 
-    try (Connection connection = DriverManager.getConnection(
-        data.getUrlDb(), data.getUserDb(), data.getPasswordDb())){
+    if (size > 0) {
+      statement.execute("TRUNCATE \"TEST\"");
+    }
+  }
 
-      Statement statement = connection.createStatement();
+  private static void fillDB(Statement statement) throws SQLException {
+    StringBuilder query = new StringBuilder().append("INSERT INTO \"TEST\" VALUES (");
 
-      //    ==============
+    for (int i = 0; i < data.getN(); i++) {
+      int field = (i + 1);
 
-      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM \"TEST\"");
-      resultSet.next();
+      query.append(field).append(")");
 
-      int size = Integer.parseInt(resultSet.getString(1));
-
-      if (size > 0) {
-        statement.execute("TRUNCATE \"TEST\"");
+      if (field < data.getN()) {
+        query.append(", (");
       }
-
-      //    ==============
-
-      StringBuilder query = new StringBuilder().append("INSERT INTO \"TEST\" VALUES (");
-
-      for (int i = 0; i < data.getN(); i++) {
-        int field = (i + 1);
-
-        query.append(field).append(")");
-
-        if (field < data.getN()) {
-          query.append(", (");
-        }
-      }
-
-      statement.execute(query.toString());
-
-      timeSQL = System.currentTimeMillis() - (timeInit + start);
-      System.out.println("SQL-таблица заполнена. Прошло ещё " + timeSQL/1000 + " сек.");
-
-      //    ==============
-
-      File file1 = new File("1.xml");
-      file1.createNewFile();
-
-      FileWriter writer = new FileWriter(file1, false);
-      writer.write("<entries>\n");
-
-      resultSet = statement.executeQuery("SELECT * FROM \"TEST\"");
-
-      while (resultSet.next()){
-        writer.write("  <entry>\n");
-        writer.write("    <field>" + resultSet.getString("field") + "</field>\n");
-        writer.write("  </entry>\n");
-      }
-
-      writer.write("</entries>\n");
-      writer.flush();
-
-      time1xml = System.currentTimeMillis() - (timeSQL + start);
-      System.out.println("1.xml создан. Прошло ещё " + time1xml/1000 + " сек.");
-    } catch (SQLException e) {
-      System.err.println("Нет соединения с базой данных");
-    } catch (IOException e) {
-      e.printStackTrace();
     }
 
-    //    ==============
+    statement.execute(query.toString());
+  }
 
+  private static void createXml(Statement statement) throws IOException, SQLException {
+    File file1 = new File("1.xml");
+    file1.createNewFile();
+
+    FileWriter writer = new FileWriter(file1, false);
+    writer.write("<entries>\n");
+
+    ResultSet resultSet = statement.executeQuery("SELECT * FROM \"TEST\"");
+
+    while (resultSet.next()) {
+      writer.write("  <entry>\n");
+      writer.write("    <field>" + resultSet.getString("field") + "</field>\n");
+      writer.write("  </entry>\n");
+    }
+
+    writer.write("</entries>\n");
+    writer.flush();
+  }
+
+  private static File transformXmlToXml(String styleSheetFile) throws TransformerException {
     TransformerFactory factory = TransformerFactory.newInstance();
     StreamSource xlsStream = new StreamSource(
-        Application.class.getResource("TransformXML.xsl").toExternalForm());
+        Application.class.getResource(styleSheetFile).toExternalForm());
 
     StreamSource in = new StreamSource("1.xml");
-
-    File file2 = new File("2.xml");
+    File resultXml = new File("2.xml");
 
     try {
-      file2.createNewFile();
+      resultXml.createNewFile();
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    StreamResult out = new StreamResult(file2);
+    StreamResult out = new StreamResult(resultXml);
+    Transformer transformer = factory.newTransformer(xlsStream);
+    transformer.transform(in, out);
 
-    try {
-      Transformer transformer = factory.newTransformer(xlsStream);
+    return resultXml;
+  }
 
-      transformer.transform(in, out);
+  private static Entries parseXML(File sourceXml) throws Exception {
+    Serializer serializer = new Persister();
+    return serializer.read(Entries.class, sourceXml);
+  }
 
-      Serializer serializer = new Persister();
-      Entries entries = serializer.read(Entries.class, file2);
-
-      time2xml = System.currentTimeMillis() - (time1xml + start);
-      System.out.println("2.xml создан. Прошло ещё " + time2xml/1000 + " сек.");
-
-      //    ==============
-
-      int sum = 0;
-
-      for (Entry e : entries.entries) {
-        sum += e.field;
-      }
-
-      System.out.println(sum);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-    timeSum = System.currentTimeMillis() - (time2xml + start);
-    System.out.println("Сумма подсчитана. Прошло ещё " + timeSum/1000 + " сек.");
-
-    //    ==============
-
-    timeAll = System.currentTimeMillis() - start;
-    System.out.println("Всего прошло " + timeAll/1000 + " сек.");
-
-    //    ==============
-
-    int sum = 0;
+  private static void checkSum(long sum) {
+    long checkSum = 0;
 
     for (int i = 1; i <= data.getN(); i++) {
-      sum += i;
+      checkSum += i;
     }
 
-    System.out.println("Проверочная сумма: " + sum);
+    if (sum == checkSum) {
+      System.out.println("Сумма подсчитана верно.");
+    } else {
+      System.out.println("Сумма подсчитана неверно.");
+    }
   }
 }
